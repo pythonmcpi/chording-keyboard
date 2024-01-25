75409a7be1f553ac3a722df7436e5f652ec23a1c
Pipythonmc <47196755+pythonmcpi@users.noreply.github.com>
2024-01-25 11:03:09 -0800

    Debounced chorder variant with the correct pins

---
+++ dbc_pins.py
@@ -0,0 +1,123 @@
+import board
+import digitalio as dio
+import microcontroller as mc
+import time
+import supervisor
+
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+
+kb = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(kb)
+
+DEBOUNCE_DURATION = 30_000_000
+
+CONFIG_INPUT_PINS = [
+    mc.pin.GPIO16, # Pinky finger
+    mc.pin.GPIO18, # Ring finger
+    mc.pin.GPIO26, # Middle finger
+    mc.pin.GPIO28, # Index finger
+    mc.pin.GPIO10, # Leftmost thumb
+    mc.pin.GPIO6, # Middle thumb
+    mc.pin.GPIO3, # Rightmost thumb
+]
+
+KEYS = []
+
+CAN_CHORD = False
+CHORD_TABLE = {
+    (True, False, False, False, False, False, False): "a",
+    (False, True, False, False, False, False, False): "b",
+    (True, True, False, False, False, False, False): "c",
+    (False, False, True, False, False, False, False): "d",
+    (True, False, True, False, False, False, False): "e",
+    (False, True, True, False, False, False, False): "f",
+    (True, True, True, False, False, False, False): "g",
+}
+
+def prime_chord():
+    global CAN_CHORD
+
+    CAN_CHORD = True
+
+def send_chord():
+    global CAN_CHORD
+
+    if not CAN_CHORD:
+        # We need this, otherwise keyups for one chord would trigger multiple chords
+        return
+
+    # CHORD_TABLE uses True if the key was pressed, while the actual pins
+    # say True if the key is not pressed. So we invert it.
+    current_key_comb = tuple(not key.lastvalue for key in KEYS)
+
+    try:
+        # We can index dicts with immutable values such as tuples
+        text_to_send = CHORD_TABLE[current_key_comb]
+    except KeyError:
+        # No chord found
+        # Don't reset CAN_CHORD
+        print("No chord found:", current_key_comb)
+        return
+
+    CAN_CHORD = False
+
+    print("Sending text:", text_to_send)
+
+    layout.write(text_to_send)
+
+class Key:
+    def __init__(self, cpin):
+        self.cpin = cpin
+
+        pin = dio.DigitalInOut(cpin)
+        pin.direction = dio.Direction.INPUT
+        pin.pull = dio.Pull.UP
+
+        self.pin = pin
+        self.lastvalue = pin.value
+        self.value = self.lastvalue
+        self.lastchange = time.monotonic_ns()
+
+    def update(self):
+        self.value = self.pin.value
+        if self.value != self.lastvalue:
+            tnow = time.monotonic_ns()
+            if tnow < self.lastchange + DEBOUNCE_DURATION:
+                return False
+
+            self.lastchange = tnow
+
+            self.onstatechange()
+            self.lastvalue = self.value
+
+            return True
+
+        return False
+
+    def onstatechange(self):
+        print("Change in state detected on pin", self.cpin, "to", "on" if self.value else "off", "(last value was", "on" if self.lastvalue else "off", ")")
+
+        if self.value:
+            # Keyup (nonpressed = high)
+            # Look for a chord match and send it
+            send_chord()
+        else:
+            # Keydown
+            # Allow a chord to be sent
+            prime_chord()
+
+for cpin in CONFIG_INPUT_PINS:
+    KEYS.append(Key(cpin))
+
+led = dio.DigitalInOut(board.LED)
+led.direction = dio.Direction.OUTPUT
+
+finished = False
+
+while not finished:
+    led.value = KEYS[0].pin.value
+    for key in KEYS:
+        key.update()
